f85465e6fe548239240fc8db4f12e6b3325e77b7
Tyler Cecchi <thcecchi@gmail.com>
2015-02-05 13:04:30 -0500

    created object literal

---
+++ main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  chatApp.init();
+});
+
+var chatApp = {
+
+  config: {
+    url: "http://tiy-fee-rest.herokuapp.com/collections/closetalkers"
+  },
+
+  init: function () {
+    chatApp.initStyle();
+    chatApp.initEvents();
+  },
+
+  initStyle: function () {
+
+  },
+
+  initEvents: function () {
+
+
+
+  },
+
+  render: function (data, tmpl, $el) {
+
+
+  },
+
+  renderAllListItems: function () {
+
+  },
+
+  createListItem: function (items) {
+
+  },
+
+  deleteListItem: function (id) {
+
+
+
+  },
+
+  updateListItem: function (id, items) {
+
+
+
+}
